3b5fb49339534b1a4469a40cc8e719f7731b1613
alexbursak <alexbursak3@gmail.com>
2019-09-29 15:18:10 +0100

    lesson 31

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
-    return React.createElement('div', null, 'h1', 'sup babe? I am an FBI agent. Beep beep');
-  // return (
-  //   <div className="App">
-  //     <h1>Yo</h1>
-  //   </div>
-  // );
+    // return React.createElement('div', null, 'h1', 'sup babe? I am an FBI agent. Beep beep');
+  return (
+    <div className="App">
+        <h1>Yo</h1>
+        <Person />
+    </div>
+  );
 }
 
 export default App;
