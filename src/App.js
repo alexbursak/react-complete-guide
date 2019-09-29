6da1da395126ab4f343b932f23b6b7a32cb3cfc4
alexbursak <alexbursak3@gmail.com>
2019-09-29 15:29:39 +0100

    lesson 34

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Person from './Person/Person';
 
 function App() {
-    // return React.createElement('div', null, 'h1', 'sup babe? I am an FBI agent. Beep beep');
   return (
     <div className="App">
         <h1>Yo</h1>
+        <Person />
+        <Person />
         <Person />
     </div>
   );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Person = () => {
     return (
         <div>
-            <p>I am a Person component.</p>
+            <p>I am a Person and I am a {Math.floor(Math.random() * 100)} years old.</p>
         </div>
     );
 };
