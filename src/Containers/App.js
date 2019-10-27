a96d4ef3ebd7c0591ea6850a58ecacfd8fe74ffe
alexbursak <alexbursak3@gmail.com>
2019-10-27 17:22:53 +0000

    lesson 85

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
+import Person from '../Components/Persons/Person/Person';
 
 class App extends Component {
 
@@ -25,7 +25,7 @@ class App extends Component {
             return person.id === personId;
         });
 
-        // copying the person and assigning new value 
+        // copying the person and assigning new value
         let person = {...this.state.persons[personIndex]};
         person.name = event.target.value;
 
