b21106d23be736cb30d5a9c0949c012661881fff
alexbursak <alexbursak3@gmail.com>
2020-04-24 16:05:33 +0100

    lesson 102

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Person.css';
+import Aux from "../../../hoc/Aux";
 
 const Person = (props) => {
     const spanStyle = {
@@ -9,13 +10,15 @@ const Person = (props) => {
     };
 
     return (
-        <div className={classes.Person} >
+        // <div className={classes.Person} >
+        <Aux>
             <p>I am a {props.name} and I am a {props.age} years old.
                 <span style={spanStyle} onClick={props.clickDeleteHandler}>X</span>
             </p>
             <p>{props.children}</p>
             <input type="text" onChange={props.nameChangedHandler} value={props.name}/>
-        </div>
+        </Aux>
+        // </div>
     );
 };
 
