3a2e8346c0bba45ef3f21191b8bc03c690dd796c
alexbursak <alexbursak3@gmail.com>
2020-04-25 12:50:37 +0100

    lesson 110

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 import WithClassAux from "../../../hoc/WithClassAux";
 import PropTypes from 'prop-types';
 
-const Person = (props) => {
-    const spanStyle = {
-        cursor: 'pointer',
-        marginLeft: '15px',
-        fontWeight: 'bold'
-    };
+class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
 
-    return (
-        <WithClassAux classes={classes.Person} >
-            <p>I am a {props.name} and I am a {props.age} years old.
-                <span style={spanStyle} onClick={props.clickDeleteHandler}>X</span>
-            </p>
-            <p>{props.children}</p>
-            <input
-                type="text"
-                onChange={props.nameChangedHandler}
-                value={props.name}
-            />
-        </WithClassAux>
-    );
-};
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus()
+    }
+
+    render() {
+        const spanStyle = {
+            cursor: 'pointer',
+            marginLeft: '15px',
+            fontWeight: 'bold'
+        };
+
+        return (
+            <WithClassAux classes={classes.Person} >
+                <p>I am a {this.props.name} and I am a {this.props.age} years old.
+                    <span style={spanStyle} onClick={this.props.clickDeleteHandler}>X</span>
+                </p>
+                <p>{this.props.children}</p>
+                <input
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
+                    type="text"
+                    onChange={this.props.nameChangedHandler}
+                    value={this.props.name}
+                />
+            </WithClassAux>
+        );
+    }
+}
 
 Person.propTypes = {
     click: PropTypes.func,
