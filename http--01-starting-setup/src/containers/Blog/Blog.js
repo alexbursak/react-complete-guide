4cdc9e19955b3abd1025c4cca998dbe3e6dca221
alexbursak <alexbursak3@gmail.com>
2020-05-02 19:15:03 +0100

    lesson 171

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -16,7 +16,6 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then((response) => {
-                console.log(response);
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
                     return {...post, author: 'Alex'}
--- http--01-starting-setup/src/index.js
+++ http--01-starting-setup/src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(requestConf => {
+    console.log(requestConf);
+    // Edit request here if needed
+    return requestConf;
+}, error => {
+    console.log(error);
+
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(responseConf => {
+    console.log(responseConf);
+    // Edit response here if needed
+    return responseConf;
+}, error => {
+    console.log(error);
+
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
