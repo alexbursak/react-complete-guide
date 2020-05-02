01bfb8e46130aabb72ab120c0f01297ec12704c9
alexbursak <alexbursak3@gmail.com>
2020-05-02 19:26:08 +0100

    lesson 174

---
+++ http--01-starting-setup/src/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+instance.defaults.headers.common['Authorization'] = 'MY_TOKEN_INST';
+
+export default instance;
