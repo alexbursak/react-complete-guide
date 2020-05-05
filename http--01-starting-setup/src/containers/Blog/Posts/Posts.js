425178a02f13911940709e3e40d989290f28186a
alexbursak <alexbursak3@gmail.com>
2020-05-05 23:57:51 +0100

    lesson 192

--- http--01-starting-setup/src/containers/Blog/Posts/Posts.js
+++ http--01-starting-setup/src/containers/Blog/Posts/Posts.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from "./Posts.css";
+import Post from "../../../components/Post/Post";
+import axios from "../../../axios";
 
-const Posts = (props) => (
-    <div className={classes.Posts}>
-        {props.children}
-    </div>
-);
+class Posts extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+    }
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then((response) => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {...post, author: 'Alex'}
+                });
+                this.setState({posts: updatedPosts})
+            })
+            .catch(error => {
+                this.setState({error: true})
+            });
+    }
+
+    postClickedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render() {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong.</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map((post) => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postClickedHandler(post.id)}
+                />
+            });
+        }
+
+        return (
+            <div className={classes.Posts}>
+                <section className="Posts">
+                    {posts}
+                </section>
+            </div>
+        );
+    }
+}
 
 export default Posts;
