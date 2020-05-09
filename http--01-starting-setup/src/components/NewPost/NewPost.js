031e4921eda2914f5fcfdcc0e4bb108eb35ab65a
alexbursak <alexbursak3@gmail.com>
2020-05-09 16:42:03 +0100

    lesson 212

--- http--01-starting-setup/src/components/NewPost/NewPost.js
+++ http--01-starting-setup/src/components/NewPost/NewPost.js
@@ -7,8 +7,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Alex',
-        submitted: false
+        author: 'Alex'
+        // submitted: false
     }
 
     postDataHandler = () => {
@@ -20,14 +20,15 @@ class NewPost extends Component {
         axios.post('/posts', post)
             .then(response => {
                 console.log(response);
-                this.setState({submitted: true})
+                this.props.history.push('/posts');
+                // this.setState({submitted: true})
             });
     }
 
     render () {
         return (
             <div className="NewPost">
-                {this.state.submitted ? <Redirect to="posts" /> : null}
+                {/*{this.state.submitted ? <Redirect to="posts" /> : null}*/}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -7,6 +7,10 @@ import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 // import FullPost from "../../components/FullPost/FullPost";
 
 class Blog extends Component {
+    state = {
+        auth: true
+    }
+
     render() {
         return (
             <div className="Blog">
@@ -31,7 +35,7 @@ class Blog extends Component {
                 </header>
                 {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
                 <Switch>
-                    <Route path="/new-post" component={NewPost}/>
+                    {this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
                     <Route path="/posts" component={Posts}/>
                     <Redirect from="/" to="/posts" />
                 </Switch>
